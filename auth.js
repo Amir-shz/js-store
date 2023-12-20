bb73db0f0e501db4489bd52472ec2feae2a1ce84
amir-shz <amirsharif2079@gmail.com>
2023-12-21 00:05:28 +0330

    username and password validation

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { postData } from "./utils/httpReq.js";
 import { setCookie, getCookie } from "./utils/cookie.js";
 import authHandler from "./utils/authorization.js";
+import { validateForm } from "./utils/validation.js";
 
 const inputBox = document.querySelectorAll("input");
 const loginButton = document.querySelector("button");
@@ -9,6 +10,10 @@ const submitHandler = async (event) => {
   event.preventDefault();
   const username = inputBox[0].value;
   const password = inputBox[1].value;
+
+  const validation = validateForm(username, password);
+  if (!validation) return;
+
   const data = { username, password };
   const response = await postData("auth/login", data);
   setCookie("token", response.token);
